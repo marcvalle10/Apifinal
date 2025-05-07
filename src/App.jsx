de3508de71ac6bd08af4998c0dd7cecd50bd4a5d
marcvalle10 <134673564+marcvalle10@users.noreply.github.com>
2025-05-07 16:29:01 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,13 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      
       </p>
-      <p>El pepin está en su era pilates</p>
+   
     </>
   )
 }
